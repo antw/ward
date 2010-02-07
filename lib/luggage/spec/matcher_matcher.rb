60c73da5bcf9bdea7c46890cc986ca2681dc1a91
Anthony Williams <anthony@ninecraft.com>
2010-02-07 21:23:10 +0000

    Add pass_matcher_with and fail_matcher_with spec matchers.

---
+++ lib/luggage/spec/matcher_matcher.rb
@@ -0,0 +1,51 @@
+module Luggage
+  module Spec
+    module MatcherMatcher
+
+      EXPECTED_PASS = "expected %s matcher to pass (expected: %s, " \
+                      "actual: %s), but it failed"
+
+      EXPECTED_FAIL = "expected %s matcher to fail (expected: %s, " \
+                      "actual: %s), but it passed"
+
+      # An RSpec matcher which tests that the Luggage matcher passes with a
+      # particular value.
+      #
+      ::Spec::Matchers.define :pass_matcher_with do |value|
+        match do |matcher|
+          matcher.matches?(value)
+        end
+
+        failure_message_for_should do |matcher|
+          Luggage::Spec::MatcherMatcher::EXPECTED_PASS % [
+            matcher.class.inspect, matcher.expected.inspect, value.inspect ]
+        end
+
+        failure_message_for_should_not do |matcher|
+          Luggage::Spec::MatcherMatcher::EXPECTED_FAIL % [
+            matcher.class.inspect, matcher.expected.inspect, value.inspect ]
+        end
+      end
+
+      # An RSpec matcher which tests that the Luggage matcher fails with a
+      # particular value.
+      #
+      ::Spec::Matchers.define :fail_matcher_with do |value|
+        match do |matcher|
+          not matcher.matches?(value)
+        end
+
+        failure_message_for_should do |matcher|
+          Luggage::Spec::MatcherMatcher::EXPECTED_FAIL % [
+            matcher.class.inspect, matcher.expected.inspect, value.inspect ]
+        end
+
+        failure_message_for_should_not do |matcher|
+          Luggage::Spec::MatcherMatcher::EXPECTED_PASS % [
+            matcher.class.inspect, matcher.expected.inspect, value.inspect ]
+        end
+      end
+
+    end # MatcherMatcher
+  end # Spec
+end # Luggage
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,11 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rubygems'
-require 'luggage'
 require 'spec'
 require 'spec/autorun'
+
+require 'luggage'
+require 'luggage/spec'
 
 Spec::Runner.configure do |config|
 
